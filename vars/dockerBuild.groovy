827d7497c2d6d54cdefee3c942d6b3ed8a675906
Kiruthik25 <113363817+Kiruthik25@users.noreply.github.com>
2026-07-27 23:12:30 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,23 +1,40 @@
+// def call(Map config = [:]) {
+
+//     def imageName = config.imageName ?: 'kiruthik25/cvwj-devsecops-demo'
+//     def imageTag  = config.imageTag  ?: env.BUILD_NUMBER ?: 'latest'
+//     def registryCredentialsId = config.credentialsId ?: 'dockerhub-creds'
+
+//     sh """
+//         echo "Building Docker image ${imageName}:${imageTag}..."
+//         docker build -t ${imageName}:${imageTag} -t ${imageName}:latest .
+//     """
+
+//     withCredentials([usernamePassword(
+//         credentialsId: registryCredentialsId,
+//         usernameVariable: 'DOCKER_USER',
+//         passwordVariable: 'DOCKER_PASS'
+//     )]) {
+//         sh """
+//             echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+//             docker push ${imageName}:${imageTag}
+//             docker push ${imageName}:latest
+//         """
+//     }
+// }
+
 def call(Map config = [:]) {
 
     def imageName = config.imageName ?: 'kiruthik25/cvwj-devsecops-demo'
     def imageTag  = config.imageTag  ?: env.BUILD_NUMBER ?: 'latest'
-    def registryCredentialsId = config.credentialsId ?: 'dockerhub-creds'
 
+    // Assumes the EC2 agent is already logged into Docker Hub
+    // (e.g. `docker login` was run once manually on the box).
     sh """
         echo "Building Docker image ${imageName}:${imageTag}..."
         docker build -t ${imageName}:${imageTag} -t ${imageName}:latest .
-    """
 
-    withCredentials([usernamePassword(
-        credentialsId: registryCredentialsId,
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
-        sh """
-            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
-        """
-    }
+        echo "Pushing Docker image..."
+        docker push ${imageName}:${imageTag}
+        docker push ${imageName}:latest
+    """
 }
