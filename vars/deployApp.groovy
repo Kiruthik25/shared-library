e94965d6a75e631d85519bb267059cd37844f348
Kiruthik25 <113363817+Kiruthik25@users.noreply.github.com>
2026-07-27 23:13:39 +0530

    Simplify deployment by removing kubeCredentialsId
    Removed kubeCredentialsId handling and added note about kubeconfig.

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,13 +1,27 @@
+// def call(Map config = [:]) {
+
+//     def manifestPath = config.manifestPath ?: 'deployment/deployment.yaml'
+//     def kubeCredentialsId = config.kubeCredentialsId ?: 'kubeconfig-creds'
+
+//     withCredentials([file(credentialsId: kubeCredentialsId, variable: 'KUBECONFIG')]) {
+//         sh """
+//             echo "Deploying application to Kubernetes..."
+//             kubectl apply -f ${manifestPath}
+//             kubectl rollout status deployment/cvwj-devsecops-demo --timeout=120s
+//         """
+//     }
+// }
+
+
 def call(Map config = [:]) {
 
     def manifestPath = config.manifestPath ?: 'deployment/deployment.yaml'
-    def kubeCredentialsId = config.kubeCredentialsId ?: 'kubeconfig-creds'
 
-    withCredentials([file(credentialsId: kubeCredentialsId, variable: 'KUBECONFIG')]) {
-        sh """
-            echo "Deploying application to Kubernetes..."
-            kubectl apply -f ${manifestPath}
-            kubectl rollout status deployment/cvwj-devsecops-demo --timeout=120s
-        """
-    }
+    // Assumes the EC2 agent already has a working kubeconfig
+    // (e.g. ~/.kube/config set up manually on the box, pointing at the cluster).
+    sh """
+        echo "Deploying application to Kubernetes..."
+        kubectl apply -f ${manifestPath}
+        kubectl rollout status deployment/cvwj-devsecops-demo --timeout=120s
+    """
 }
