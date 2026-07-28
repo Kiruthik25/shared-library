4ca79eaa33eb05bbc7acf46a437b78bea0d97a80
Kiruthik25 <113363817+Kiruthik25@users.noreply.github.com>
2026-07-28 11:04:30 +0530

    Refactor dockerBuild.groovy for improved functionality

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,38 +1,38 @@
+def call(Map config = [:]) {
+
+    def imageName = config.imageName ?: 'kiruthik067/cvwj-devsecops-demo'
+    def imageTag  = config.imageTag  ?: env.BUILD_NUMBER ?: 'latest'
+    def registryCredentialsId = config.credentialsId ?: 'dockerhub-creds'
+
+    sh """
+        echo "Building Docker image ${imageName}:${imageTag}..."
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest .
+    """
+
+    withCredentials([usernamePassword(
+        credentialsId: registryCredentialsId,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker push ${imageName}:${imageTag}
+            docker push ${imageName}:latest
+        """
+    }
+}
+
 // def call(Map config = [:]) {
 
 //     def imageName = config.imageName ?: 'kiruthik25/cvwj-devsecops-demo'
 //     def imageTag  = config.imageTag  ?: env.BUILD_NUMBER ?: 'latest'
-//     def registryCredentialsId = config.credentialsId ?: 'dockerhub-creds'
 
+//     // Assumes the EC2 agent is already logged into Docker Hub
+//     // (e.g. `docker login` was run once manually on the box).
 //     sh """
 //         echo "Building Docker image ${imageName}:${imageTag}..."
 //         docker build -t ${imageName}:${imageTag} -t ${imageName}:latest .
+
+//         echo " Docker image... built ....."
 //     """
-
-//     withCredentials([usernamePassword(
-//         credentialsId: registryCredentialsId,
-//         usernameVariable: 'DOCKER_USER',
-//         passwordVariable: 'DOCKER_PASS'
-//     )]) {
-//         sh """
-//             echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
-//             docker push ${imageName}:${imageTag}
-//             docker push ${imageName}:latest
-//         """
-//     }
 // }
-
-def call(Map config = [:]) {
-
-    def imageName = config.imageName ?: 'kiruthik25/cvwj-devsecops-demo'
-    def imageTag  = config.imageTag  ?: env.BUILD_NUMBER ?: 'latest'
-
-    // Assumes the EC2 agent is already logged into Docker Hub
-    // (e.g. `docker login` was run once manually on the box).
-    sh """
-        echo "Building Docker image ${imageName}:${imageTag}..."
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest .
-
-        echo " Docker image... built ....."
-    """
-}
